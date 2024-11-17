c8ed7de26e08c1aad34967f9c5b220cc29577480
abhi-drops <abhinand7667@gmail.com>
2024-11-18 01:51:07 +0530

    pwa

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,44 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    VitePWA({registerType:'autoUpdate'})
+  plugins: [
+    react(),
+    VitePWA({
+       registerType: 'autoUpdate',
+       manifest: {
+        name: 'StepsAway',
+        short_name: 'StepsAway',
+        description: 'Explore our City like never before!',
+        theme_color: '#E5F7F5',
+        display: "standalone", // Ensures full-screen behavior
+        orientation: "portrait", // Locks the app to portrait mode
+        "icons": [
+          {
+            "src": "/pwa-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "any"
+          },
+          {
+            "src": "/pwa-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "any"
+          },
+          {
+            "src": "/pwa-maskable-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "maskable"
+          },
+          {
+            "src": "/pwa-maskable-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "maskable"
+          }
+        ],
+      }
+      }),
   ],
 })
